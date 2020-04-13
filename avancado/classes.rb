a968afe3114bf635b0585b1d492bcad36e78aaa2
Marcos <marcos.andrade@protonmail.com>
2020-04-13 17:57:41 -0300

    C8 - Aula sobre construtores em Ruby

--- avancado/classes.rb
+++ avancado/classes.rb
@@ -1,18 +1,25 @@
 # Conta
 # Teste
 class Conta
-  attr_accessor :saldo
+  attr_accessor :saldo, :nome
+
+  def initialize(nome)
+    self.saldo = 0.0
+    self.nome = nome
+  end
 
   def deposita(valor)
     # recurso para invocar atributo
     self.saldo += valor
-    puts "Depositando a quantida de #{valor} reais"
+    puts "Depositando a quantida de #{valor} reais na conta de #{self.nome}"
   end
 end
 
-c = Conta.new
+c = Conta.new('Marcos')
 
-c.saldo = 0
 c.deposita(100.00)
-
 puts c.saldo
+
+c.deposita(10.00)
+puts c.saldo
+puts c.nome
