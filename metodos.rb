8314353bb6e0d0922b1d8760f4f5524a2536bc98
Marcos <marcos.andrade@protonmail.com>
2020-04-13 16:35:09 -0300

    C3 - Aula sobre métodos em Ruby

---
+++ metodos.rb
@@ -0,0 +1,22 @@
+def diga_ola(nome)
+    puts 'Olá, ' + nome
+end
+
+diga_ola('Marcos')
+
+# def retorna_nome
+#     'Marcos'
+# end
+
+# resultado = retorna_nome
+# puts resultado
+
+# def soma(v1, v2)
+#     total = v1 + v2
+#     total
+# end
+
+# res = soma(10,25)
+# puts res
+
+# selecionar ctrl ; comenta tudo
