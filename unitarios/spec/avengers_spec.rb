569f63b8efdcf36ca3dc54a90fd9565f33f64bec
Marcos <marcos.andrade@protonmail.com>
2020-04-16 21:17:27 -0300

    C14 - Aprendendo um pouco mais de TDD com rspec

--- unitarios/spec/avengers_spec.rb
+++ unitarios/spec/avengers_spec.rb
@@ -17,4 +17,38 @@ describe AvengersHeadQuarter do
     hq.put('Spiderman')
     expect(hq.list).to eql ['Spiderman']
   end
+
+  it 'deve adicionar uma lista de vingadores' do
+    hq = AvengersHeadQuarter.new
+    hq.put('Thor')
+    hq.put('Hulk')
+    hq.put('Spiderman')
+    # to include para saber se existe o registro
+    expect(hq.list).to include 'Thor'
+    res = hq.list.size > 0
+    expect(res).to be true
+  end
+
+  it 'thor deve ser o primeiro da lista' do
+    hq = AvengersHeadQuarter.new
+    hq.put('Thor')
+    hq.put('Hulk')
+    hq.put('Spiderman')
+    expect(hq.list).to start_with('Thor')
+  end
+
+  it 'ironman deve ser o último da lista' do
+    hq = AvengersHeadQuarter.new
+    hq.put('Thor')
+    hq.put('Hulk')
+    hq.put('Spiderman')
+    hq.put('Ironman')
+    expect(hq.list).to end_with('Ironman')
+  end
+
+  it 'deve contar o sobrenome' do
+    avenger = 'Peter Parker'
+    expect(avenger).to match(/Parker/)
+    expect(avenger).not_to match(/Marcos/)
+  end
 end
